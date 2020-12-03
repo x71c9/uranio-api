4c77a45550402471f7ed66a5a36eca1aaf4e8602
x71c9 <108585118+x71c9@users.noreply.github.com>
2020-12-03 18:01:05 +0100

    status: not working. before refactoring.

---
+++ src/typ/config.ts
@@ -0,0 +1,20 @@
+/**
+ * Atom configuration types module
+ *
+ * @packageDocumentation
+ */
+
+// import urn_core from 'urn-core';
+
+// export type WebAtomConfig = {
+//   atoms: WebAtomList
+// }
+
+// type WebAtomList = {
+//   [k:string]: WebAtomDefinition
+// }
+
+// export interface WebAtomDefinition extends urn_core.types.CoreAtomDefinition {
+//   url: string
+// }
+
