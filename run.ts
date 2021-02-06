ebb60e72fec0407213a3e68975583f62034f50a6
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-02-06 11:33:20 +0100

    validate request on standard route.

--- run.ts
+++ run.ts
@@ -13,5 +13,5 @@ import urn_web from 'urn_web';
 const express_service = urn_web.service.express.create();
 
 express_service.listen(3000, () => {
-	console.log(`Listening on port 3000...`);
+	urn_log.debug(`Listening on port 3000...`);
 });
