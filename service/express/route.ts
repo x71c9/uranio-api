a6b21f71cc9a90c51ef663ae4f5b6fe105becbf3
Andrea Reni <mail@nbl7.net>
2021-02-06 11:33:20 +0100

    validate request on standard route.

--- service/express/route.ts
+++ service/express/route.ts
@@ -16,6 +16,7 @@ import {AtomName, Query} from '../../types';
 
 import {ExpressQueryParam} from './types';
 
+
 export function create(atom_name:AtomName):express.Router{
 	
 	urn_log.fn_debug(`Create Express Router`);
@@ -23,6 +24,9 @@ export function create(atom_name:AtomName):express.Router{
 	const router = express.Router();
 	
 	router.get('/', async (req, res) => {
+		
+		_only_valid_query_keys(req.query, ['filter','options']);
+		_empty(req.body, 'body');
 		
 		const urn_bll = urn_core.bll.create_basic(atom_name);
 		
@@ -36,9 +40,13 @@ export function create(atom_name:AtomName):express.Router{
 	
 	router.get('/:id', async (req, res) => {
 		
+		_only_valid_param_keys(req.param, ['id']);
+		_only_valid_query_keys(req.query, ['filter','options']);
+		_empty(req.body, 'body');
+		
 		const urn_bll = urn_core.bll.create_basic(atom_name);
 		
-		const options = _process_request_options(req.query.depth);
+		const options = _process_request_options(req.query.options);
 		
 		const res_find = await urn_bll.find_by_id(req.params.id, options);
 		
@@ -47,6 +55,9 @@ export function create(atom_name:AtomName):express.Router{
 	});
 	
 	router.post('/', async (req, res) => {
+		
+		_empty(req.params, 'params');
+		_empty(req.query, 'query');
 		
 		const urn_bll = urn_core.bll.create_basic(atom_name);
 		
@@ -58,6 +69,9 @@ export function create(atom_name:AtomName):express.Router{
 	
 	router.post('/:id', async (req, res) => {
 		
+		_only_valid_param_keys(req.param, ['id']);
+		_empty(req.query, 'query');
+		
 		const urn_bll = urn_core.bll.create_basic(atom_name);
 		
 		const res_find = await urn_bll.update_by_id(req.params.id, req.body);
@@ -67,6 +81,10 @@ export function create(atom_name:AtomName):express.Router{
 	});
 	
 	router.delete('/:id', async (req, res) => {
+		
+		_only_valid_param_keys(req.param, ['id']);
+		_empty(req.query, 'query');
+		_empty(req.body, 'body');
 		
 		const urn_bll = urn_core.bll.create_basic(atom_name);
 		
@@ -101,4 +119,42 @@ function _process_request_options<A extends AtomName>(options:ExpressQueryParam)
 	}
 	throw urn_exc.create(`INVALID_OPTIONS`, `Invalid options`);
 }
+
+function _only_valid_query_keys(query:any, valid_query_keys:string[])
+		:void{
+	if(typeof query === 'object'){
+		for(const [k] of Object.entries(query)){
+			if(!valid_query_keys.includes(k)){
+				const err_msg = `Invalid request query parameters [${k}]`;
+				throw urn_exc.create(`INVALID_QUERY`, err_msg);
+			}
+		}
+	}
+}
+
+function _only_valid_param_keys(params:any, valid_params_keys:string[])
+		:void{
+	if(typeof params === 'object'){
+		for(const [k] of Object.entries(params)){
+			if(!valid_params_keys.includes(k)){
+				const err_msg = `Invalid parameters [${k}]`;
+				throw urn_exc.create(`INVALID_PARAMETERS`, err_msg);
+			}
+		}
+	}
+}
+
+function _empty(p:any, param_name:string)
+		:true{
+	if(!p){
+		return true;
+	}
+	if(typeof p === 'object' && Object.keys(p).length === 0){
+		return true;
+	}
+	const err_msg = `Invalid request ${param_name}. ${param_name[0].toUpperCase() + param_name.slice(1)} should be empty.`;
+	throw urn_exc.create(`INVALID_BODY`, err_msg);
+}
+
+
 
