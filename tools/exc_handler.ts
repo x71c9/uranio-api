1ef510316b12f4c7ff632478939dad1993c2d821
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-02-08 13:37:46 +0100

    Added exception handler when starting.

---
+++ tools/exc_handler.ts
@@ -0,0 +1,44 @@
+/**
+ * Module for handling exceptions and rejected promises
+ *
+ * @packageDocumentation
+ */
+
+/*
+ * Function for handling exception.
+ * It should log the exception and stop the application.
+ *
+ * @params ex - The exception
+ */
+function handleException(ex:Error):void{
+	// urn_console.error('URANIO UncaughtException', ex);
+	console.log(ex);
+	process.exit(1);
+}
+
+/*
+ * Function for handling rejected promises.
+ * It should log the rejected promise and stop the application.
+ *
+ * @param reason - the reason
+ * @param promise - the promise
+ */
+function handleRejectedPromise(reason:any, promise:Promise<any>):void{
+	// urn_console.error('URANIO UnhandledRejection', reason, promise);
+	console.log(reason, promise);
+	process.exit(1);
+}
+
+/*
+ * Function that will assign to process uncaughtException handleException and
+ * to unhandledRejection handleRejectionPromise functions.
+ */
+function registerExceptionHandler():void {
+	process.on('uncaughtException', handleException);
+	process.on('unhandledRejection', handleRejectedPromise);
+}
+
+/*
+ * Calling registerExceptionHandler
+ */
+registerExceptionHandler();
