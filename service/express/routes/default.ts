029cc52e0a52f6e50a42b2845c1bc26eaaabdef0
Andrea Reni <mail@nbl7.net>
2021-02-23 11:42:55 +0100

    implemented default route definition.

--- service/express/routes/default.ts
+++ service/express/routes/default.ts
@@ -16,6 +16,8 @@ import {
 	Book
 } from '../../../types';
 
+import {return_default_routes} from '../../routes';
+
 import {route_middlewares} from '../mdlw';
 
 export function create_route<A extends AtomName>(atom_name:A)
@@ -27,9 +29,39 @@ export function create_route<A extends AtomName>(atom_name:A)
 	
 	const atom_api = atom_book[atom_name].api as Book.Definition.Api;
 	
-	if(atom_api && atom_api.routes){
+	if(!atom_api){
+		return router;
+	}
+	
+	for(const [route_name, route_def] of Object.entries(return_default_routes(atom_name))){
 		
-		// TODO load default routes
+		switch(route_def.method){
+			case RouteMethod.GET: {
+				router.get(route_def.url, route_middlewares(atom_name, route_name));
+				break;
+			}
+			case RouteMethod.POST: {
+				router.post(route_def.url, route_middlewares(atom_name, route_name));
+				break;
+			}
+			case RouteMethod.DELETE: {
+				router.delete(route_def.url, route_middlewares(atom_name, route_name));
+				break;
+			}
+		}
+		
+	}
+	
+	if(!atom_api.routes){
+		
+		atom_api.routes = return_default_routes(atom_name);
+		
+	}else{
+		
+		atom_api.routes = {
+			...return_default_routes(atom_name),
+			...atom_api.routes
+		};
 		
 		for(const [route_name, route_def] of Object.entries(atom_api.routes)){
 			
