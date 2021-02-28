4c19810082cdb7ffdead74bc57a2b4542d8e0616
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-02-28 11:23:59 +0100

    uranio module.

---
+++ uranio.ts
@@ -0,0 +1,9 @@
+/**
+ * Export combined core, web and types module into uranio
+ *
+ * @packageDocumentation
+ */
+
+import * as uranio from './urn/';
+
+export default uranio;
---
+++ urn/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Index module for URANIO Web
+ *
+ * @packageDocumentation
+ */
+
+import core from 'urn_core';
+
+import web from 'urn_web';
+
+import * as types from '../types';
+
+export {core, web, types};
