a21e867ad7dc74257e73f4caf587e2082e3fcbde
Andrea Reni <mail@nbl7.net>
2021-03-07 10:43:30 +0100

    updated book.

--- books.ts
+++ books.ts
@@ -1,40 +1,44 @@
+/**
+ * Required web books
+ *
+ * @packageDocumentation
+ */
 
-import urn_core from 'urn_core';
+import uranio from 'uranio';
 
 export const atom = {
-	...urn_core.types.required_books.atom,
 	error: {
 		connection: 'log',
 		security: {
-			type: urn_core.types.BookSecurityType.UNIFORM,
-			_r: urn_core.types.BookPermissionType.NOBODY
+			type: uranio.types.BookSecurityType.UNIFORM,
+			_r: uranio.types.BookPermissionType.NOBODY
 		},
 		properties: {
 			status: {
-				type: urn_core.types.BookPropertyType.INTEGER,
+				type: uranio.types.BookPropertyType.INTEGER,
 				label: 'Status'
 			},
 			msg: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Message'
 			},
 			error_code: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Error Code'
 			},
 			error_msg: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Error Message'
 			},
 			request: {
-				type: urn_core.types.BookPropertyType.ATOM,
+				type: uranio.types.BookPropertyType.ATOM,
 				label: 'Request',
 				atom: 'request',
 				delete_cascade: true,
 				optional: true
 			},
 			stack: {
-				type: urn_core.types.BookPropertyType.LONG_TEXT,
+				type: uranio.types.BookPropertyType.LONG_TEXT,
 				label: 'Stack',
 				optional: true
 			}
@@ -43,40 +47,40 @@ export const atom = {
 	request: {
 		connection: 'log',
 		security: {
-			type: urn_core.types.BookSecurityType.UNIFORM,
-			_r: urn_core.types.BookPermissionType.NOBODY
+			type: uranio.types.BookSecurityType.UNIFORM,
+			_r: uranio.types.BookPermissionType.NOBODY
 		},
 		properties: {
 			url: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'URL'
 			},
 			ip: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'IP'
 			},
 			params: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Params',
 				optional: true
 			},
 			query: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Query',
 				optional: true
 			},
 			body: {
-				type: urn_core.types.BookPropertyType.LONG_TEXT,
+				type: uranio.types.BookPropertyType.LONG_TEXT,
 				label: 'Body',
 				optional: true
 			},
 			atom_name: {
-				type: urn_core.types.BookPropertyType.TEXT,
+				type: uranio.types.BookPropertyType.TEXT,
 				label: 'Atom name',
 				on_error: () => {return 'generic_atom';}
 			},
 			auth_action: {
-				type: urn_core.types.BookPropertyType.ENUM_STRING,
+				type: uranio.types.BookPropertyType.ENUM_STRING,
 				label: 'Auth action',
 				values: ['READ', 'WRITE'],
 				on_error: () => {return 'READ';}
@@ -86,13 +90,11 @@ export const atom = {
 } as const;
 
 export const bll = {
-	...urn_core.types.required_books.bll,
 	error: {},
 	request: {}
 } as const;
 
 export const api = {
-	...urn_core.types.required_books.api,
 	error: {
 		api:{
 			url: 'errors'
