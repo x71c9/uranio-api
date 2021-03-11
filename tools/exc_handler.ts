032a4e4c7b5b9db69debebda27bb1be20232c1ba
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-03-11 08:41:17 +0100

    moved exec handler into service class instance.

--- tools/exc_handler.ts
+++ tools/exc_handler.ts
@@ -14,21 +14,22 @@ const bll_errors = urn_core.bll.log.create('error');
  *
  * @params ex - The exception
  */
-async function handleException(ex:Error)
-		:Promise<void>{
-	console.error(ex);
-	try {
-		await bll_errors.insert_new({
-			status: 500,
-			msg: 'UnhandledException',
-			error_code: '500',
-			error_msg: ex.message,
-			stack: ex.stack
-		});
-	}catch(ex){
-		// TODO
-	}
-	process.exit(1);
+function handle_exception(service_name:string):(...args:any[]) => any {
+	return async (ex:Error):Promise<void> => {
+		console.error(service_name, ex);
+		try {
+			await bll_errors.insert_new({
+				status: 500,
+				msg: `[${service_name}] UnhandledException`,
+				error_code: '500',
+				error_msg: ex.message,
+				stack: ex.stack
+			});
+		}catch(ex){
+			// TODO
+		}
+		process.exit(1);
+	};
 }
 
 /*
@@ -38,33 +39,36 @@ async function handleException(ex:Error)
  * @param reason - the reason
  * @param promise - the promise
  */
-async function handleRejectedPromise(reason:any, promise:Promise<any>)
-		:Promise<void>{
-	console.error(reason, promise);
-	try {
-		await bll_errors.insert_new({
-			status: 510,
-			msg: 'UnhandledRejectedPromise',
-			error_code: '510',
-			error_msg: JSON.stringify(reason),
-			stack: JSON.stringify(promise)
-		});
-	}catch(ex){
-		// TODO
-	}
-	process.exit(1);
+function handle_rejected_promise(service_name:string):(...args:any[]) => any {
+	return async (reason:any, promise:Promise<any>):Promise<void> => {
+		console.error(service_name, reason, promise);
+		try {
+			await bll_errors.insert_new({
+				status: 510,
+				msg: `[${service_name}] UnhandledRejectedPromise`,
+				error_code: '510',
+				error_msg: JSON.stringify(reason),
+				stack: JSON.stringify(promise)
+			});
+		}catch(ex){
+			// TODO
+		}
+		process.exit(1);
+	};
 }
 
 /*
- * Function that will assign to process uncaughtException handleException and
+ * Function that will assign to process uncaughtException handle_exception and
  * to unhandledRejection handleRejectionPromise functions.
  */
-function registerExceptionHandler():void {
-	process.on('uncaughtException', handleException);
-	process.on('unhandledRejection', handleRejectedPromise);
+export function register_exception_handler(name:string):void {
+	
+	process.on('uncaughtException', handle_exception(name));
+	process.on('unhandledRejection', handle_rejected_promise(name));
+	
 }
 
 /*
- * Calling registerExceptionHandler
+ * Calling register_exception_handler
  */
-registerExceptionHandler();
+// register_exception_handler();
