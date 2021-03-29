6cbf9396116f6ac21a0d5d1864ff20051ef5f591
Andrea Reni <mail@nbl7.net>
2021-03-29 16:06:14 +0200

    ServiceType. Select service type.

--- run.ts
+++ run.ts
@@ -10,7 +10,7 @@ urn_log.defaults.log_level = urn_log.LogLevel.FUNCTION_DEBUG;
 
 import urn_web from './index';
 
-const express_service = urn_web.service.express.create();
+const express_service = urn_web.service.create('express');
 
 express_service.listen(3000, () => {
 	urn_log.debug(`Listening on port 3000...`);
