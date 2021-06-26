ae8cf86a3c7e8d7cdf0b38d990354fbf4edc124f
Andrea Reni <mail@nbl7.net>
2021-06-26 13:16:53 +0200

    client module export.

---
+++ client.ts
@@ -0,0 +1,14 @@
+/**
+ * Index module for URANIO Web CLIENT
+ *
+ * @packageDocumentation
+ */
+
+import core from './core/client';
+
+import * as types from './export';
+
+export default {
+	core,
+	types
+};
