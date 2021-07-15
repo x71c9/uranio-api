20e15d577721a3d766609926bfbd8bdf4a27ba78
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-07-15 18:51:05 +0200

    started lambda.

---
+++ lambda/types.ts
@@ -0,0 +1,79 @@
+/**
+ * Lambda types module
+ *
+ * @packageDocumentation
+ */
+
+// From https://github.com/netlify/functions/tree/main/src/function 
+
+export interface Event {
+	rawURL: string,
+	rawQuery: string,
+	path: string,
+	httpMethod: string,
+	headers: EventHeaders,
+	multiValueHeaders: EventMultiValueHeaders,
+	queryStringParameters: EventQueryStringParameters | null,
+	multiValueQueryStringParametes: EventMultiValueQueryStringParameters | null,
+	body: string | null,
+	isBase64Encoded: boolean
+}
+
+interface EventHeaders {
+	[name:string]: string | undefined
+}
+
+interface EventMultiValueHeaders {
+	[name:string]: string[] | undefined
+}
+
+interface EventQueryStringParameters {
+	[name:string]: string | undefined
+}
+
+interface EventMultiValueQueryStringParameters {
+	[name:string]: string[] | undefined
+}
+
+// From https://docs.aws.amazon.com/lambda/latest/dg/nodejs-context.html
+
+export interface Context {
+	callbackWaitsForEmptyEventLoop: boolean,
+	functionName: string
+	functionVersion: string
+	invokedFunctionArn: string
+	memoryLimitInMB: string
+	awsRequestId: string
+	logGroupName: string
+	logStreamName: string
+	identity?: {[key: string]: any}
+	clientContext?: {[key: string]: any}
+	getRemainingTimeInMillis(): number
+}
+
+export type Headers = {
+	[header:string]: boolean | number | string
+}
+
+export type MultiValueHeaders = {
+	[header:string]: ReadonlyArray<boolean | number | string>
+}
+
+export interface HandlerResponse {
+	statusCode: number
+	headers?: Headers
+	multiValueHeaders?: MultiValueHeaders
+	body?:string
+	isBase64Encoded?: boolean
+}
+
+export interface HandlerCallback {
+	(error: any, response: Response): void
+}
+
+export interface Handler {
+	(event: Event, context: Context, callback: HandlerCallback): void | Response | Promise<Response>
+}
+
+
+
--- main.ts
+++ main.ts
@@ -12,9 +12,12 @@ import * as types from './types';
 
 import * as service from './service/';
 
+import * as lambda from './lambda/';
+
 export {
 	urn_lib as lib,
 	core,
+	lambda,
 	types,
 	service
 };
