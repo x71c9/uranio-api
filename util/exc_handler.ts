ec4115aaef71ec0a9bd337777f898bd5328007a9
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-07-26 20:16:44 +0200

    do not wait for storing error.

--- util/exc_handler.ts
+++ util/exc_handler.ts
@@ -14,18 +14,20 @@ import urn_core from 'uranio-core';
  *
  * @params ex - The exception
  */
-function handle_exception(service_name:string, bll_errors:urn_core.bll.BLL<'error'>)
+function handle_exception(service_name:string, bll_errors?:urn_core.bll.BLL<'error'>)
 		:(...args:any[]) => any {
 	return async (ex:Error):Promise<void> => {
 		console.error(service_name, ex);
 		try {
-			await bll_errors.insert_new({
-				status: 500,
-				msg: `[${service_name}] UnhandledException`,
-				error_code: '500',
-				error_msg: ex.message,
-				stack: ex.stack
-			});
+			if(bll_errors){
+				bll_errors.insert_new({
+					status: 500,
+					msg: `[${service_name}] UnhandledException`,
+					error_code: '500',
+					error_msg: ex.message,
+					stack: ex.stack
+				});
+			}
 		}catch(ex){
 			// TODO
 		}
@@ -40,18 +42,20 @@ function handle_exception(service_name:string, bll_errors:urn_core.bll.BLL<'erro
  * @param reason - the reason
  * @param promise - the promise
  */
-function handle_rejected_promise(service_name:string, bll_errors:urn_core.bll.BLL<'error'>)
+function handle_rejected_promise(service_name:string, bll_errors?:urn_core.bll.BLL<'error'>)
 		:(...args:any[]) => any {
 	return async (reason:any, promise:Promise<any>):Promise<void> => {
 		console.error(service_name, reason, promise);
 		try {
-			await bll_errors.insert_new({
-				status: 510,
-				msg: `[${service_name}] UnhandledRejectedPromise`,
-				error_code: '510',
-				error_msg: JSON.stringify(reason),
-				stack: JSON.stringify(promise)
-			});
+			if(bll_errors){
+				bll_errors.insert_new({
+					status: 510,
+					msg: `[${service_name}] UnhandledRejectedPromise`,
+					error_code: '510',
+					error_msg: JSON.stringify(reason),
+					stack: JSON.stringify(promise)
+				});
+			}
 		}catch(ex){
 			// TODO
 		}
@@ -63,7 +67,7 @@ function handle_rejected_promise(service_name:string, bll_errors:urn_core.bll.BL
  * Function that will assign to process uncaughtException handle_exception and
  * to unhandledRejection handleRejectionPromise functions.
  */
-export function register_exception_handler(name:string, bll_errors:urn_core.bll.BLL<'error'>):void {
+export function register_exception_handler(name:string, bll_errors?:urn_core.bll.BLL<'error'>):void {
 	
 	process.on('uncaughtException', handle_exception(name, bll_errors));
 	process.on('unhandledRejection', handle_rejected_promise(name, bll_errors));
