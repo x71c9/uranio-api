e95903dba635948851e7fc21c828e26d532f2393
Andrea Reni <mail@nbl7.net>
2021-09-08 17:51:55 +0200

    added book route import.

--- typ/route.ts
+++ typ/route.ts
@@ -6,7 +6,7 @@
 
 import {AtomName} from '../cln/types';
 
-import {dock_book} from 'uranio-books/dock';
+import {dock_book} from 'uranio-books/routes';
 
 import {default_routes} from '../routes/client';
 
