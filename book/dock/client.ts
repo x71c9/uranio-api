728fb2fda8223f85a0182d45298c42cf537aaa9e
Andrea Reni <mail@nbl7.net>
2021-12-07 09:49:25 +0100

    refactored book module.

--- book/dock/client.ts
+++ book/dock/client.ts
@@ -4,4 +4,15 @@
  * @packageDocumentation
  */
 
-export * from './common';
+import * as book_dock from 'uranio-core/book/dock/client';
+
+export * from 'uranio-core/book/dock/client';
+
+import {AtomName} from '../../types';
+
+import {Book} from '../../typ/book_cln';
+
+export function get_definition<A extends AtomName>(atom_name:A)
+		:Book.Definition.Dock{
+	return book_dock.get_definition(atom_name) as Book.Definition.Dock;
+}
