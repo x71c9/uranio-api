e6c88c8ce371cb08c343072a7697704ff277259a
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-12-12 18:14:16 +0100

    fixed book dock module.

--- book/dock/client.ts
+++ book/dock/client.ts
@@ -4,15 +4,22 @@
  * @packageDocumentation
  */
 
-import * as book_dock from 'uranio-core/book/dock/client';
+// import * as book_dock from 'uranio-core/book/dock/client';
 
-export * from 'uranio-core/book/dock/client';
+// export * from 'uranio-core/book/dock/client';
+
+import {dock_book} from 'uranio-books/dock';
 
 import {AtomName} from '../../cln/types';
 
 import {Book} from '../../typ/book_cln';
 
+// export function get_definition<A extends AtomName>(atom_name:A)
+//     :Book.Definition.Dock{
+//   return book_dock.get_definition(atom_name) as Book.Definition.Dock;
+// }
+
 export function get_definition<A extends AtomName>(atom_name:A)
 		:Book.Definition.Dock{
-	return book_dock.get_definition(atom_name) as Book.Definition.Dock;
+	return dock_book[atom_name].dock as Book.Definition.Dock;
 }
--- book/dock/server.ts
+++ book/dock/server.ts
@@ -4,15 +4,22 @@
  * @packageDocumentation
  */
 
-import * as book_dock from 'uranio-core/book/dock/';
+// import * as book_dock from 'uranio-core/book/dock/';
 
-export * from 'uranio-core/book/dock/';
+// export * from 'uranio-core/book/dock/';
+
+import {dock_book} from 'uranio-books/dock';
 
 import {AtomName} from '../../types';
 
 import {Book} from '../../typ/book_srv';
 
+// export function get_definition<A extends AtomName>(atom_name:A)
+//     :Book.Definition.Dock{
+//   return book_dock.get_definition(atom_name) as Book.Definition.Dock;
+// }
+
 export function get_definition<A extends AtomName>(atom_name:A)
 		:Book.Definition.Dock{
-	return book_dock.get_definition(atom_name) as Book.Definition.Dock;
+	return dock_book[atom_name].dock as Book.Definition.Dock;
 }
