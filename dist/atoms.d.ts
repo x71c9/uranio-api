abdd5be32adb2b5a96cc9c20b5523f872d8a3afe
Andrea Reni <mail@nbl7.net>
2022-02-17 20:33:41 +0100

    updated generate script.

--- dist/atoms.d.ts
+++ dist/atoms.d.ts
@@ -3,41 +3,42 @@
  *
  * @packageDocumentation
  */
+import core from 'uranio-core';
 export declare const atom_book: {
     readonly error: {
         readonly plural: "errors";
         readonly read_only: true;
         readonly connection: "log";
         readonly security: {
-            readonly type: any;
-            readonly _r: any;
+            readonly type: core.types.BookSecurity.UNIFORM;
+            readonly _r: core.types.BookPermission.NOBODY;
         };
         readonly properties: {
             readonly status: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.INTEGER;
                 readonly label: "Status";
             };
             readonly msg: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Message";
             };
             readonly error_code: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Error Code";
             };
             readonly error_msg: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Error Message";
             };
             readonly request: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.ATOM;
                 readonly label: "Request";
                 readonly atom: "request";
                 readonly delete_cascade: true;
                 readonly optional: true;
             };
             readonly stack: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.LONG_TEXT;
                 readonly label: "Stack";
                 readonly optional: true;
             };
@@ -51,83 +52,83 @@ export declare const atom_book: {
         readonly read_only: true;
         readonly connection: "log";
         readonly security: {
-            readonly type: any;
-            readonly _r: any;
+            readonly type: core.types.BookSecurity.UNIFORM;
+            readonly _r: core.types.BookPermission.NOBODY;
         };
         readonly properties: {
             readonly full_path: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Full path";
             };
             readonly route_path: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Route path";
                 readonly optional: true;
             };
             readonly atom_path: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Atom path";
                 readonly optional: true;
             };
             readonly connection_path: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Connection path";
                 readonly optional: true;
             };
             readonly method: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.ENUM_STRING;
                 readonly label: "Method";
                 readonly values: readonly ["GET", "POST", "DELETE"];
                 readonly optional: true;
             };
             readonly atom_name: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Atom name";
                 readonly optional: true;
                 readonly on_error: () => string;
             };
             readonly route_name: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Route name";
                 readonly optional: true;
             };
             readonly params: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Params";
                 readonly optional: true;
             };
             readonly query: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "Query";
                 readonly optional: true;
             };
             readonly headers: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.LONG_TEXT;
                 readonly label: "Headers";
                 readonly optional: true;
             };
             readonly body: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.LONG_TEXT;
                 readonly label: "Body";
                 readonly optional: true;
             };
             readonly file: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "File";
                 readonly optional: true;
             };
             readonly ip: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.TEXT;
                 readonly label: "IP";
                 readonly optional: true;
             };
             readonly is_auth: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.BINARY;
                 readonly label: "Is auth";
                 readonly optional: true;
             };
             readonly auth_action: {
-                readonly type: any;
+                readonly type: core.types.BookProperty.ENUM_STRING;
                 readonly label: "Auth action";
                 readonly values: readonly ["READ", "WRITE", "AUTH"];
                 readonly on_error: () => string;
--- src/util/generate.ts
+++ src/util/generate.ts
@@ -8,10 +8,7 @@ import fs from 'fs';
 
 import core from 'uranio-core';
 
-// import {urn_util, urn_exception, urn_log} from 'urn-lib';
 import {urn_log} from 'urn-lib';
-
-// const urn_exc = urn_exception.init(`REGISTER_MODULE`, `Register module.`);
 
 import {schema} from '../sch/index';
 
@@ -21,54 +18,48 @@ import * as types from '../types';
 
 import {default_routes} from '../routes/client';
 
+let urn_generate_base_schema = `./types/schema.d.ts`;
+let urn_generate_output_dir = `.`;
+
 export function generate():void{
 	
 	urn_log.debug('Generating uranio api schema...');
 	
 	core.util.generate();
 	
-	const atom_book = book.get_all_definitions();
+	const text = _generate_schema_text();
+	_save_schema_declaration_file(text);
 	
-	// const atom_names:string[] = [];
-	// const auth_names:string[] = [];
-	// const log_names:string[] = [];
-	// for(const [atom_name, atom_def] of Object.entries(atom_book)){
-	//   atom_names.push(atom_name);
-	//   if(atom_def.authenticate === true){
-	//     auth_names.push(atom_name);
-	//   }
-	//   if(atom_def.connection === 'log'){
-	//     log_names.push(atom_name);
-	//   }
-	// }
-	
-	let txt = '';
-	txt += _generate_route_name(atom_book);
-	txt += _generate_route_url(atom_book);
-	txt += _generate_route_query_param(atom_book);
-	
-	let output_path = '.';
-	let base_schema = './schema/index.d.ts';
+	urn_log.debug(`API Schema generated.`);
+}
+
+function _save_schema_declaration_file(text:string){
 	for(const argv of process.argv){
 		const splitted = argv.split('=');
 		if(
-			splitted[0] === 'urn_generate_output'
-			&& typeof splitted[1] === 'string'
-			&& splitted[1] !== ''
-		){
-			output_path = splitted[1];
-		}else if(
 			splitted[0] === 'urn_generate_base_schema'
 			&& typeof splitted[1] === 'string'
 			&& splitted[1] !== ''
 		){
-			base_schema = splitted[1];
+			urn_generate_base_schema = splitted[1];
+		}else if(
+			splitted[0] === 'urn_generate_output_dir'
+			&& typeof splitted[1] === 'string'
+			&& splitted[1] !== ''
+		){
+			urn_generate_output_dir = splitted[1];
 		}
 	}
-	// const caller_path = caller();
-	_replace_text(base_schema, output_path, txt);
-	
-	urn_log.debug(`API Schema generated.`);
+	_replace_text(urn_generate_base_schema, urn_generate_output_dir, text);
+}
+
+function _generate_schema_text(){
+	const atom_book = book.get_all_definitions();
+	let txt = '';
+	txt += _generate_route_name(atom_book);
+	txt += _generate_route_url(atom_book);
+	txt += _generate_route_query_param(atom_book);
+	return txt;
 }
 
 function _generate_route_query_param(atom_book:types.Book){
@@ -200,10 +191,8 @@ function _route_custom_name<A extends schema.AtomName>(atom_def:types.Book.Defin
 	return route_names.join(' | ');
 }
 
-function _replace_text(base_schema_path:string, output_path:string, txt:string){
-	if(!fs.existsSync(output_path)){
-		fs.writeFileSync(output_path, '');
-	}
+function _replace_text(base_schema_path:string, output_dir_path:string, txt:string){
+	
 	const data = fs.readFileSync(base_schema_path, {encoding: 'utf8'});
 	
 	const split_text = '\texport {};/** --uranio-generate-end */';
@@ -215,6 +204,6 @@ function _replace_text(base_schema_path:string, output_path:string, txt:string){
 	new_data += split_text;
 	new_data += data_splitted[1];
 	
-	fs.writeFileSync(output_path, new_data);
+	fs.writeFileSync(`${output_dir_path}/schema.d.ts`, new_data);
 }
 
