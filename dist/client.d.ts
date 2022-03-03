a51d0cfa11c3dde6afcbfb4a91975f89c5653fa4
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-03-03 10:35:23 +0100

    removed export register.

--- dist/client.d.ts
+++ dist/client.d.ts
@@ -3,7 +3,6 @@
  *
  * @packageDocumentation
  */
-export * from './client/register';
 import * as urn_api from './client/main';
 export * from './client/main';
 export default urn_api;
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@
  * @packageDocumentation
  */
 
-export * from './server/register';
+// export * from './server/register';
 
 import * as urn_api from './server/main';
 
