24b00378144fbc3afe92b34fad28a16b6010a1ca
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-03-07 20:48:41 +0100

    updated conf types.

--- src/conf/defaults.ts
+++ src/conf/defaults.ts
@@ -22,6 +22,12 @@ export const api_config:Required<Configuration> = {
 	
 	service_port: 3000,
 	
+	service_dev_protocol: 'http',
+	
+	service_dev_domain: 'localhost',
+	
+	service_dev_port: 7777,
+	
 	lambda: 'netlify',
 	
 	prefix_api: '/uranio/api',
--- src/typ/conf.ts
+++ src/typ/conf.ts
@@ -14,14 +14,17 @@ type RequiredConfigParams = {
 }
 
 type OptionalConfigParam = {
+	request_auto_limit: number
 	service: ServiceName
-	lambda: LambdaName
-	prefix_api: string
-	prefix_log: string
 	service_protocol: string
 	service_domain: string
 	service_port: number
-	request_auto_limit: number
+	service_dev_protocol: string
+	service_dev_domain: string
+	service_dev_port: number
+	lambda: LambdaName
+	prefix_api: string
+	prefix_log: string
 }
 
 export type Configuration =
