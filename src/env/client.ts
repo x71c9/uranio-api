db413a8e2ee5152d030eb5b39752169d72f9a817
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-03-11 12:43:39 +0100

    import core client.

--- src/env/client.ts
+++ src/env/client.ts
@@ -6,7 +6,7 @@
 
 import {urn_context} from 'urn-lib';
 
-import core from 'uranio-core';
+import core_client from 'uranio-core/client';
 
 import {api_client_env} from '../client/default_env';
 
@@ -19,7 +19,7 @@ const urn_ctx = urn_context.create<Required<ClientEnvironment>>(
 );
 
 export function is_production():boolean{
-	return core.env.is_production();
+	return core_client.env.is_production();
 }
 
 export function get<k extends keyof ClientEnvironment>(
