72b2c2d4d9c6ed8794905a87c739aa508cd57ef6
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-05-13 13:33:29 +0200

    removed comments dev.

--- dist/dev.js
+++ dist/dev.js
@@ -12,18 +12,5 @@ const server_1 = __importDefault(require("./server"));
 server_1.default.init();
 const service = server_1.default.service.create();
 service.listen(async () => {
-    // const error_bll = uranio.core.bll.basic.create(`error`);
-    // console.log(error_bll);
-    // const res = await error_bll.find({});
-    // console.log(res);
 });
-// const error_bll = uranio.core.bll.basic.create(`error`);
-// console.log(error_bll);
-// error_bll.find({}).then((res) => {
-//   console.log(res);
-// }).catch(err => console.error(err));
-// const basic_bll = uranio.core.bll.basic.create('error');
-// basic_bll.find_by_id('625bddfde2f65c4d52a57892', {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
 //# sourceMappingURL=dev.js.map
--- src/dev.ts
+++ src/dev.ts
@@ -9,18 +9,4 @@ uranio.init();
 
 const service = uranio.service.create();
 service.listen(async () => {
-	// const error_bll = uranio.core.bll.basic.create(`error`);
-	// console.log(error_bll);
-	// const res = await error_bll.find({});
-	// console.log(res);
 });
-// const error_bll = uranio.core.bll.basic.create(`error`);
-// console.log(error_bll);
-// error_bll.find({}).then((res) => {
-//   console.log(res);
-// }).catch(err => console.error(err));
-
-// const basic_bll = uranio.core.bll.basic.create('error');
-// basic_bll.find_by_id('625bddfde2f65c4d52a57892', {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
